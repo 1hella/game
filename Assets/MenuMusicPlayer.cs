700090c2e22823f563f7757b04de75ba13452001
ampaulg <ampaulgill@gmail.com>
2023-09-01 12:24:08 -0700

    Added music to main menu, playable by pressing 1 on keypad

---
+++ Assets/MenuMusicPlayer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusicPlayer : MonoBehaviour
+{
+    public AudioClip intro1;
+    public AudioClip loop1;
+    private AudioSource audioSource;
+    private bool started = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            audioSource.clip = intro1;
+            audioSource.Play();
+            started = true;
+        }
+
+        if (started && (!audioSource.isPlaying))
+        {
+            audioSource.clip = loop1;
+            audioSource.Play();
+            audioSource.loop = true;
+        }
+    }
+}
