197b53a1defe265a79113831b86e0b34f6de26db
Stephen Wanhella <stephen.wanhella@gmail.com>
2023-09-02 23:43:22 -0700

    Implement multiple tasks, fade out and in, and reset days

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,22 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+
 
 public class GameController : MonoBehaviour
 {
-    //private TaskController taskController;
-    private int numDay;
+    private int score; // number of tasks completed
+    public TaskController taskController;
+    public DialogueManager dialogController;
+    public UIController uiController;
+    public PlayerController playerController;
+    private const int NUM_TASKS = 4;
+
+    public int Score
+    {
+        get => score; 
+        set
+        {
+            score = value;
+            if (score % NUM_TASKS == 0 && score > 0)
+            {
+                EndDay();
+            }
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        //this.taskController = new TaskController();
-        this.numDay = 1;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void EndDay()
+    {
+        uiController.FadeToBlack(true);
+    }
+
+    public void RestartLevel()
+    {
+        playerController.ResetLocation();
+        dialogController.PlayNext();
+        taskController.ResetAllTasks();
     }
 }
