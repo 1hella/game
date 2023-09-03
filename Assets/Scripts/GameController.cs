77b8ed327d77cef13a6819777b2c8efb7571fee7
Stephen Wanhella <stephen.wanhella@gmail.com>
2023-09-02 18:34:25 -0700

    Create GameController.cs

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //private TaskController taskController;
+    private int numDay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this.taskController = new TaskController();
+        this.numDay = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
